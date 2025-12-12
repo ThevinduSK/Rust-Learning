2a4d59cb39ad412123d613b905dafc5138f65790
ThevinduSK <thevindukalubowila123@gmail.com>
2025-12-12 08:24:39 +0530

    Restructured the codebase and added the codes for control flow / looping

---
+++ Concepts/loops/src/while.rs
@@ -0,0 +1,41 @@
+// fn main() {
+//     let mut number = 3;
+
+//     while number != 0 {
+//         println!("{number}!");
+
+//         number -= 1;
+//     }
+
+//     println!("LIFTOFF!!!");
+// }
+
+// looping through an array
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+//     let mut index = 0;
+
+//     while index < 5 {
+//         println!("the value is: {}", a[index]);
+
+//         index += 1;
+//     }
+// }
+
+
+// alternative way to loop through an array
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+
+//     for element in a {
+//         println!("the value is: {element}");
+//     }
+// }
+
+// for loop as a while loop
+fn main() {
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
